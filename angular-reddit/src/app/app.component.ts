3779171559971583c0c21001792f106bc73ce995
ramon-richard <ramon.richard@iisgalvanimi.edu.it>
2021-12-20 09:36:48 +0000

    parte ispeziona

--- angular-reddit/src/app/app.component.ts
+++ angular-reddit/src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+    let t = title.value;
+    let v= link.value;
+    console.log(`Adding article title: `+t+ ` and link: `+ v);
+    console.log(`Adding article title: `+title.value+ ` and link: `+ link.value);
+    console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+    return false;
+  }
 
 }
